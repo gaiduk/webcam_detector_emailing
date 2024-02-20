88cae4e2d9786edc3f9ceff021b0324efddab437
algorithmdelta <algorithmdelta@gmail.com>
2024-02-20 14:57:18 +0200

    Email is completed

--- emailing.py
+++ emailing.py
@@ -1,2 +1,30 @@
-def send_mail():
-    print("Email sent")
+import os
+import smtplib
+import ssl
+import imghdr
+from email.message import EmailMessage
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object detected by webcamPythonApp"
+    email_message.set_content("Hello, we detected something by your web camera")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    host = "smtp.gmail.com"
+    port = 465
+    username = "gaidukpetro@gmail.com"
+    password = os.getenv("PORTF_PASS_EMAIL")
+    receiver = "gaidukpetro@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, email_message.as_string())
+
+
+if __name__ == "__main__":
+    send_email("images/webcam_app_22.png")
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import cv2
 import time
-from emailing import send_mail
+import glob
+import os
+from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_foulder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
+
+clean_foulder_thread = Thread(target=clean_foulder)
+clean_foulder_thread.daemon = True
+clean_foulder_thread.start()
 
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -21,7 +37,7 @@ while True:
 
     thrash_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thrash_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
+    # cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -32,13 +48,20 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/webcam_app_{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_obj = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
     print(status_list)
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_mail()
+        email_thread = Thread(target=send_email, args=(image_with_obj, ))
+        email_thread.daemon = True
+        email_thread.start()
 
     cv2.imshow("Video", frame)
 
